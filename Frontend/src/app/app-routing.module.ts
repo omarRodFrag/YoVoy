1e32272136d109efadb2930bf516e879d6919813
omarRodFrag <omar.rodriguez@linkthinks.com>
2025-04-07 21:43:07 -0600

    Menu Hamburguesa

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MapaComponent } from './components/mapa/mapa.component';
+import { UnidadesComponent } from './components/unidades/unidades.component';
+import { PersonalComponent } from './components/personal/personal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/mapa', pathMatch: 'full' }, // Ruta inicial
+  { path: 'mapa', component: MapaComponent },
+  { path: 'unidades', component: UnidadesComponent },
+  { path: 'personal', component: PersonalComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
